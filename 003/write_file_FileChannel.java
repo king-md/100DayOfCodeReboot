9fbb2031366722c5c711aee505bb0cec9eb2c001
Michael King <gizmo.mdk+gthbcm@gmail.com>
2019-05-20 16:26:11 -0700

    make Java FileChannel version to work

--- 003/write_file_FileChannel.java
+++ 003/write_file_FileChannel.java
@@ -1,33 +1,25 @@
-// Title: Write a file using C functions (fopen, fclose, fprintf, etc.).
+// Title: Write a file using the Java FileChannel class
 // File: write_file_FileChannel.java
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class write_file_FileChannel  {
-    public static void main(String[] args)  {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    public static void main( String[] args ) throws IOException  {
+        RandomAccessFile stream = new RandomAccessFile( "example_out_003_FileChannel.txt", "rw" );
+        FileChannel channel = stream.getChannel();
+        String value = "Now is the time for all good people to come to the aid of their country.\n\n";
+        byte[] strBytes = value.getBytes();
+        ByteBuffer buffer = ByteBuffer.allocate( strBytes.length );
+        buffer.put( strBytes );
+        buffer.flip();
+
+        channel.write( buffer );
+
+        stream.close();
+        channel.close();
     }
 }
 
-
-
-
-/*
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()  {
-   char const * const outfile_name = "example_out_002.txt";
-
-   FILE* outfile_p = fopen( outfile_name, "w" );
-   if( nullptr == outfile_p )  {
-       fprintf( stderr, "Unable to open file %s.\n\n", outfile_name );
-       exit( 1 );
-   }
-   fprintf( outfile_p, "Now is the time for all good "
-      "people to come to the aid of their country.\n\n" );
-   fclose( outfile_p );
-
-   exit( 0 );
-}
-*/
