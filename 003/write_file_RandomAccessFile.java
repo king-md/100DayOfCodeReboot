4bef383adc4a3c3023c1f391f9040a923f0ccfff
Michael King <gizmo.mdk+gthbcm@gmail.com>
2019-05-20 16:12:21 -0700

    make Java RandomAccessFile version to work

--- 003/write_file_RandomAccessFile.java
+++ 003/write_file_RandomAccessFile.java
@@ -1,33 +1,15 @@
-// Title: Write a file using C functions (fopen, fclose, fprintf, etc.).
+// Title: Write a file using the Java RandomAccessFile class
 // File: write_file_RandomAccessFile.java
 
+import java.io.*;
 
 public class write_file_RandomAccessFile  {
-    public static void main(String[] args)  {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    public static void main( String[] args ) throws IOException  {
+        RandomAccessFile writer = new RandomAccessFile( "example_out_003_RandomAccessFile.txt", "rw" );
+        writer.seek( 0 );
+        writer.writeBytes( "Now is the time for all good people to come to the aid of their country.\n\n" );
+
+        writer.close();
     }
 }
 
-
-
-
-/*
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()  {
-   char const * const outfile_name = "example_out_002.txt";
-
-   FILE* outfile_p = fopen( outfile_name, "w" );
-   if( nullptr == outfile_p )  {
-       fprintf( stderr, "Unable to open file %s.\n\n", outfile_name );
-       exit( 1 );
-   }
-   fprintf( outfile_p, "Now is the time for all good "
-      "people to come to the aid of their country.\n\n" );
-   fclose( outfile_p );
-
-   exit( 0 );
-}
-*/
