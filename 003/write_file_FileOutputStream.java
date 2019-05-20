dce7b66aca93d48c4aaf232ed9b4b20305ae2bfc
Michael King <gizmo.mdk+gthbcm@gmail.com>
2019-05-20 15:50:44 -0700

    make Java FileOutputStream version to work

--- 003/write_file_FileOutputStream.java
+++ 003/write_file_FileOutputStream.java
@@ -1,33 +1,17 @@
-// Title: Write a file using C functions (fopen, fclose, fprintf, etc.).
+// Title: Write a file using the Java FileOutputStream class
 // File: write_file_FileOutputStream.java
 
+import java.io.*;
 
 public class write_file_FileOutputStream  {
-    public static void main(String[] args)  {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    public static void main( String[] args ) throws IOException  {
+        String str = "Now is the time for all good people to come to the aid of their country.";
+        FileOutputStream outputStream = new FileOutputStream( "example_out_003_FileOutputStream.txt" );
+
+        byte[] strToBytes = str.getBytes();
+        outputStream.write( strToBytes );
+
+        outputStream.close();
     }
 }
 
-
-
-
-/*
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()  {
-   char const * const outfile_name = "example_out_002.txt";
-
-   FILE* outfile_p = fopen( outfile_name, "w" );
-   if( nullptr == outfile_p )  {
-       fprintf( stderr, "Unable to open file %s.\n\n", outfile_name );
-       exit( 1 );
-   }
-   fprintf( outfile_p, "Now is the time for all good "
-      "people to come to the aid of their country.\n\n" );
-   fclose( outfile_p );
-
-   exit( 0 );
-}
-*/
