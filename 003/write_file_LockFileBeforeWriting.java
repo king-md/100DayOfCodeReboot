bac406a72502b16ee53de7be3edc05f2c07b3cd7
Michael King <gizmo.mdk+gthbcm@gmail.com>
2019-05-21 17:33:22 -0700

    make Java Lock File Before Writing version to work

--- 003/write_file_LockFileBeforeWriting.java
+++ 003/write_file_LockFileBeforeWriting.java
@@ -1,33 +1,29 @@
-// Title: Write a file using C functions (fopen, fclose, fprintf, etc.).
+// Title: Write a file using the Java Lock File Before Writing class
 // File: write_file_LockFileBeforeWriting.java
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.OverlappingFileLockException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
 
 public class write_file_LockFileBeforeWriting  {
-    public static void main(String[] args)  {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+    public static void main( String[] args ) throws IOException  {
+        RandomAccessFile stream = new RandomAccessFile( "example_out_003_LockFileBeforeWriting.txt", "rw" );
+        FileChannel channel = stream.getChannel();
+
+        FileLock lock = null;
+        try  {
+            lock = channel.tryLock();
+        } catch ( final OverlappingFileLockException e )  {
+            stream.close();
+            channel.close();
+        }
+        stream.writeChars( "Now is the time for all good people to come to the aid of their country." );
+        lock.release();
+
+        stream.close();
+        channel.close();
     }
 }
 
-
-
-
-/*
-#include <stdlib.h>
-#include <stdio.h>
-
-int main()  {
-   char const * const outfile_name = "example_out_002.txt";
-
-   FILE* outfile_p = fopen( outfile_name, "w" );
-   if( nullptr == outfile_p )  {
-       fprintf( stderr, "Unable to open file %s.\n\n", outfile_name );
-       exit( 1 );
-   }
-   fprintf( outfile_p, "Now is the time for all good "
-      "people to come to the aid of their country.\n\n" );
-   fclose( outfile_p );
-
-   exit( 0 );
-}
-*/
